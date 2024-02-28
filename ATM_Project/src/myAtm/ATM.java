3c9e5817433ecf08840ac15613c5141c240827df
Malesh Kumar <58444487+Malesh245@users.noreply.github.com>
2024-02-28 17:47:30 +0530

    Add files via upload

---
+++ ATM_Project/src/myAtm/ATM.java
@@ -0,0 +1,15 @@
+package myAtm;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu{
+	
+	public static void main(String[] args) throws IOException {
+		
+		OptionMenu optionmenu = new OptionMenu();
+		
+		optionmenu.getLogin();
+		
+	}
+
+}
